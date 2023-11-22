4f7431200c80d135cd7fdec23f7ab19973e59f46
Willem van Heemstra <wvanheemstra@icloud.com>
2023-11-22 11:35:34 +0000

    Update

--- containers/app/lego/models/instCSV.js
+++ containers/app/lego/models/instCSV.js
@@ -1,28 +1,34 @@
 MYAPP.instCSV = [
     [""],
 
-    [0, "buildDirection", "y" ],
     ["Part_3032.1"],
+
+    [0, "group", "tree"],
+    [0, "buildDirection", "y"],
     ["Part_3062.2"],
     ["Part_4733.3"],
     ["Part_3794.4"],
     ["Part_3024.10"],
 
-    [0, "buildDirection", "-x" ],
+    [0, "buildDirection", "-x"],
     ["Part_3794.6"],
     ["Part_3024.14"],
 
-    [0, "buildDirection", "-z" ],
+    [0, "buildDirection", "-z"],
     ["Part_3794.7"],
     ["Part_3024.13"],
 
-    [0, "buildDirection", "x" ],
+    [0, "buildDirection", "x"],
     ["Part_3794.5"],
     ["Part_3024.11"],
 
     [0, "buildDirection", "z"],
     ["Part_3794.9"],
     ["Part_3024.12"],
+
+    [0, "endGroup"],
+    [0, "buildDirection", "y"],
+    [0, "translate", "tree", 10],
 
     [0, "group", "house"],
     [0, "buildDirection", "y"],
@@ -31,6 +37,6 @@ MYAPP.instCSV = [
     ["Part_3039.16"],
     [0, "endGroup"],
 
-    [0, "translate", "house", 10]
+    [0, "translate", "house", 10],
 
 ];
