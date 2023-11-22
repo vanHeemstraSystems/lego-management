d65b8fff774bfba4a51060d722fa5d5372351768
Willem van Heemstra <wvanheemstra@icloud.com>
2023-11-22 11:30:45 +0000

    Update

--- containers/app/lego/models/instCSV.js
+++ containers/app/lego/models/instCSV.js
@@ -24,9 +24,13 @@ MYAPP.instCSV = [
     ["Part_3794.9"],
     ["Part_3024.12"],
 
+    [0, "group", "house"],
     [0, "buildDirection", "y"],
     ["Part_3003.8"],
     ["Part_3039.15"],
     ["Part_3039.16"],
+    [0, "endGroup"],
+
+    [0, "translate", "house", 10]
 
 ];
