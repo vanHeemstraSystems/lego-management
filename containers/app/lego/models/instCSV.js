bbdac84621baeb159e7b5fc45fa81b4feb00780d
Willem van Heemstra <wvanheemstra@icloud.com>
2023-11-23 10:16:09 +0000

    Change order for deduction

--- containers/app/lego/models/instCSV.js
+++ containers/app/lego/models/instCSV.js
@@ -2,10 +2,11 @@ L3DI.instCSV = [
     [""],
 
     [0, "buildDirection", "y"],
-    ["Part.2"],
+    ["Part.1"],
+    ["Part.3"],
 
     [0, "buildDirection", "y"],
-    ["Part.1"],
+    ["Part.2"],
 
     [0, "buildDirection", "y"],
     ["Part.0"],
