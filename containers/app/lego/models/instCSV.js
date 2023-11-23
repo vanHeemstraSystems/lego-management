46397023b9fe14e7206e2d19d3b3b8b45ff0ceef
Willem van Heemstra <wvanheemstra@icloud.com>
2023-11-23 10:22:40 +0000

    Group VNet-Subnet with symbol

--- containers/app/lego/models/instCSV.js
+++ containers/app/lego/models/instCSV.js
@@ -1,9 +1,11 @@
 L3DI.instCSV = [
     [""],
 
+    [0, "group", "VNet-Subnet with Symbol"],
     [0, "buildDirection", "y"],
     ["Part.1"],
     ["Part.3"],
+    [0, "endGroup"],
 
     [0, "buildDirection", "y"],
     ["Part.2"],
