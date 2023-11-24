30515c78831c2410288b1e6671beffa70b124b0d
Willem van Heemstra <wvanheemstra@icloud.com>
2023-11-24 13:23:44 +0000

    Show spinner

--- containers/app/lego/src/lib/L3DI.js
+++ containers/app/lego/src/lib/L3DI.js
@@ -7,10 +7,9 @@
 // [Twitter]: https://twitter.com/lk_lkaz/
 // [docs]: https://l3di.netlify.com
 // --------------------------------------------------
-
+import {useState} from 'react';
 import * as THREE from "three";
 import * as THREE_ADDONS from "three-addons";
-// import { Spinner } from "spin";  // Throws Document Undefined
 import { Tween } from "@tweenjs/tween.js";
 
 var L3DI = L3DI || { REVISION: "1" };
@@ -76,15 +75,32 @@ L3DI.Inst = function ( steps ) {
 //////////////////////////////////////////////////////////////////////
 // L3DI/setupSpinner
 //////////////////////////////////////////////////////////////////////
-L3DI.setupSpinner = function ( target, color ) {
+L3DI.setupSpinner = async function ( target, color ) {
   console.log("L3DI.setupSpinner called");
-}
+  var opts = {
+    lines: 13, // The number of lines to draw
+    length: 38, // The length of each line
+    width: 17, // The line thickness
+    radius: 45, // The radius of the inner circle
+    scale: .5,//1, // Scales overall size of the spinner
+    corners: 1, // Corner roundness (0..1)
+    color: color || '#ffffff', // CSS color or array of colors
+    fadeColor: 'transparent', // CSS color or array of colors
+    speed: 1, // Rounds per second
+    rotate: 0, // The rotation offset
+    animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    className: 'spinner', // The CSS class to assign to the spinner
+    top: '50%', // Top position relative to parent
+    left: '50%', // Left position relative to parent
+    shadow: '0 0 1px transparent', // Box-shadow for the lines
+    position: 'absolute' // Element positioning
+  };
 
-//////////////////////////////////////////////////////////////////////
-// L3DI/instCSV
-//////////////////////////////////////////////////////////////////////
-L3DI.instCSV = function () {
-    console.log("Hello from L3DI.instCSV()")
+  const Spinner = (await import('spin')).default;
+  var spinner = new Spinner(opts).spin(target);
+  return spinner;
 }
 
 export default L3DI;
