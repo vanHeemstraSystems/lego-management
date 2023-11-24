a5f6a53a8dcf716bcb1a567aaceab6d09026e6ac
Willem van Heemstra <wvanheemstra@icloud.com>
2023-11-24 13:52:05 +0000

    Add ColladaLoader

--- containers/app/lego/src/components/playground.tsx
+++ containers/app/lego/src/components/playground.tsx
@@ -5,6 +5,8 @@ import CanvasContainer from "./canvasContainer";
 import ToolContainer from "./toolContainer";
 import { connect } from "react-redux";
 import L3DI from "../lib/L3DI";
+import * as THREE from "three";
+import * as ColladaLoader from "three-collada-loader";
 import InstCSV from "../models/instCSV";
 import "./playground.scss";
 
@@ -36,12 +38,43 @@ const Playground = (props: PlaygroundProps, state: PlaygroundState) => {
         var spinner = L3DI.setupSpinner(canvasContainer, '#ffffff');
 
         var canvas = new L3DI.Canvas(canvasContainer, {
-           backgroundColor: 0x55acfc,
-           cameraDistance: 300,
-           fov: 80,
-           zoomMin: 0.5,
-           zoomMax: 3.0,
-           initialRotation: [35, 0, 0],
+            backgroundColor: 0x55acfc,
+            cameraDistance: 300,
+            fov: 80,
+            zoomMin: 0.5,
+            zoomMax: 3.0,
+            initialRotation: [35, 0, 0],
+        });
+
+        var light = new THREE.AmbientLight(0xccccff, .2);
+        canvas.addLight(light);
+
+        var light2 = new THREE.DirectionalLight(0xffffff, 0.5);
+        light2.position.set(-1, 1, 1).normalize();
+        canvas.addLight(light2);
+
+        var light3 = new THREE.DirectionalLight(0xffffff, 0.5);
+        light3.position.set(1, -1, 1).normalize();
+        canvas.addLight(light3);
+
+        var light4 = new THREE.DirectionalLight(0xffffff, 0.5);
+        light4.position.set(0, 1, 0.5).normalize();
+        canvas.addLight(light4);
+
+        var loader = new ColladaLoader();
+        loader.load(modelPath, function (data:any) {
+            var model = data.scene;
+            model.scale.set(1, 1, 1);
+            model.position.set(0, 0, 0);
+
+            model.name = ""; // Correspond to instCSV's first line. '[""],'
+
+            // setupInst(model, instCSV);
+            // setupAction(model);
+
+            canvas.addModel(model);
+
+            spinner.stop();
         });
 
         // More
