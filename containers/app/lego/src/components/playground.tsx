bdb91c838a0f71159f85fa50541bb307ff716d56
Willem van Heemstra <wvanheemstra@icloud.com>
2023-11-24 15:21:34 +0000

    Create function setupAction

--- containers/app/lego/src/components/playground.tsx
+++ containers/app/lego/src/components/playground.tsx
@@ -42,14 +42,162 @@ const Playground = (props: PlaygroundProps, state: PlaygroundState) => {
             rangeB.step = inst.getStepStep();
             rangeB.value = inst.getCurStep();
             rangeB.addEventListener("input", function (e) {
-               inst.setStep(rangeB.value);
+                inst.setStep(rangeB.value);
             }, false)
 
-            // More ...
+            var inputEvent = new Event("input");
+
+            var stepForward = function (e) {
+                e.preventDefault();
+                console.log("stepForward");
+                rangeB.value = +rangeB.value + +rangeB.step;
+                rangeB.dispatchEvent(inputEvent);
+            }
+
+            var stepBackward = function (e) {
+                e.preventDefault();
+                console.log("stepBackward");
+                rangeB.value -= rangeB.step;
+                rangeB.dispatchEvent(inputEvent);
+            }
+
+            var btn_RangeB_Minus = document.getElementById("btn_RangeB_Minus");
+            btn_RangeB_Minus.addEventListener("click", stepBackward, false)
+            btn_RangeB_Minus.addEventListener("touchstart", stepBackward, false)
+
+            var btn_RangeB_Plus = document.getElementById("btn_RangeB_Plus");
+            btn_RangeB_Plus.addEventListener("click", stepForward, false)
+            btn_RangeB_Plus.addEventListener("touchstart", stepForward, false)
+
+            var toolContainer = document.getElementById("toolContainer");
+
+            //prevent pinch-zooming
+            toolContainer.addEventListener("touchstart", function (e) {
+                if (e.touches.length >= 2) e.preventDefault();
+            }, false)
+
+            //prevent tap-zooming
+            toolContainer.addEventListener("touchend", function (e) {
+                e.preventDefault();
+            }, false)
+
+            //complex code...
+            //touchend doesn't work well in android...
+            var btn_canvas = document.getElementById("btn_canvas");
+            var isClick: Boolean;
+            var startX: any, startY: any;
+
+            var btn_canvas_touchend = function (e) {
+                if (!isClick) { return; }
+
+                isClick = false;
+
+                if (startX > btn_canvas.offsetWidth / 2) {
+                    stepForward(e);
+                } else {
+                    stepBackward(e);
+                }
+
+            }
+
+            var isMoved = function (endX: any, endY: any) {
+                return Math.abs(endX - startX) > 3 || Math.abs(endY - startY) > 3;
+            }
+
+            btn_canvas.addEventListener("touchstart", function (e) {
+                isClick = true;
+                startX = e.touches[0].pageX;
+                startY = e.touches[0].pageY;
+            }, false)
+            btn_canvas.addEventListener("mousedown", function (e) {
+                isClick = true;
+                startX = e.pageX;
+                startY = e.pageY;
+            }, false)
+            btn_canvas.addEventListener("touchmove", function (e) {
+                if (isMoved(e.touches[0].pageX, e.touches[0].pageY)) {
+                    isClick = false;
+                }
+            }, false)
+            btn_canvas.addEventListener("mousemove", function (e) {
+                if (isMoved(e.pageX, e.pageY)) {
+                    isClick = false;
+                }
+            }, false)
+            btn_canvas.addEventListener("touchend", btn_canvas_touchend, false)
+            btn_canvas.addEventListener("mouseup", btn_canvas_touchend, false)
         };
 
-        function setupAction(model) {
-            // MORE ...
+        function setupAction(model:any) {
+            console.log("model:", model);
+            var c0 = model.getObjectByName("c0");
+            var c1 = model.getObjectByName("c1");
+            var c2 = model.getObjectByName("c2");
+            var c4 = model.getObjectByName("c4");
+            var c_1 = model.getObjectByName("c_1");
+            var c_2 = model.getObjectByName("c_2");
+            var c_4 = model.getObjectByName("c_4");
+            var r2 = model.getObjectByName("r2");
+            var r4 = model.getObjectByName("r4");
+            var r_2 = model.getObjectByName("r_2");
+            var r_4 = model.getObjectByName("r_4");
+
+            var motion = {
+                clockMin: -1,
+                clockMax: 1,
+                clockStep: .01,
+                curClock: 0,
+                setClock: function (clock: any) {
+                    var clock = clock || 0;
+                    var a = Math.PI * clock * -.34;
+                    c0.rotation.set(0, a, 0);
+                    c1.rotation.set(0, a, 0);
+                    c2.rotation.set(0, a, 0);
+                    c4.rotation.set(0, a, 0);
+                    c_1.rotation.set(0, a, 0);
+                    c_2.rotation.set(0, a, 0);
+                    c_4.rotation.set(0, a, 0);
+                    r2.rotation.set(0, -a, 0);
+                    r4.rotation.set(0, -a, 0);
+                    r_2.rotation.set(0, -a, 0);
+                    r_4.rotation.set(0, -a, 0);
+                    model.rotation.set(0, -a / 2, 0);
+                    this.curClock = clock;
+                },
+            }
+
+            var rangeA = document.getElementById("rangeA");
+            rangeA.min = motion.clockMin;
+            rangeA.max = motion.clockMax;
+            rangeA.step = motion.clockStep;
+            rangeA.value = motion.curClock;
+            rangeA.addEventListener("input", function (e) {
+                motion.setClock(rangeA.value);
+            }, false)
+
+            var inputEvent = new Event("input");
+
+            var stepForward = function (e:any) {
+                e.preventDefault();
+                var unit = +rangeA.step * 10;
+                rangeA.value = (Math.round(+rangeA.value / unit) + 1) * unit;
+                rangeA.dispatchEvent(inputEvent);
+            }
+
+            var stepBackward = function (e) {
+                e.preventDefault();
+                var unit = +rangeA.step * 10;
+                rangeA.value = (Math.round(+rangeA.value / unit) - 1) * unit;
+                rangeA.dispatchEvent(inputEvent);
+            }
+
+            var btn_RangeA_Minus = document.getElementById("btn_RangeA_Minus");
+            btn_RangeA_Minus.addEventListener("click", stepBackward, false)
+            btn_RangeA_Minus.addEventListener("touchstart", stepBackward, false)
+
+            var btn_RangeA_Plus = document.getElementById("btn_RangeA_Plus");
+            btn_RangeA_Plus.addEventListener("click", stepForward, false)
+            btn_RangeA_Plus.addEventListener("touchstart", stepForward, false)
         };
 
         // On Load
