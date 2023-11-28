3898fd02958dae8f1a98f0ca375ec9eeee78014e
Willem van Heemstra <wvanheemstra@icloud.com>
2023-11-28 12:54:18 +0000

    Update

--- containers/app/lego/src/models/instCSV.js
+++
@@ -1,8 +0,0 @@
-import L3DI from "../lib/L3DI";
-
-const InstCSV = L3DI.instCSV = [
-    [""],
-
-];
-
-export default InstCSV
---
+++ containers/app/lego/src/models/instCSV.tsx
@@ -0,0 +1,48 @@
+// @ts-ignore
+var MYAPP.instCSV: any = [
+    [""],
+
+    ["Part_3032.1"],
+
+    [0, "group", "tree", 20, 3.2, -12],
+    [0, "buildDirection", "y"],
+    ["Part_3062.2"],
+    ["Part_4733.3"],
+    ["Part_3794.4"],
+    ["Part_3024.10"],
+
+    [0, "buildDirection", "-x"],
+    ["Part_3794.6"],
+    ["Part_3024.14"],
+    [0, "rotate", "tree", 0, 1, 0, -90],
+
+    [0, "buildDirection", "-z"],
+    ["Part_3794.7"],
+    ["Part_3024.13"],
+    [0, "rotate", "tree", 0, 1, 0, -90],
+
+    [0, "buildDirection", "x"],
+    ["Part_3794.5"],
+    ["Part_3024.11"],
+    [0, "rotate", "tree", 0, 1, 0, -90],
+
+    [0, "buildDirection", "z"],
+    ["Part_3794.9"],
+    ["Part_3024.12"],
+
+    [0, "endGroup"],
+    [0, "buildDirection", "y"],
+    [0, "translate", "tree", 10],
+
+    [0, "group", "house"],
+    [0, "buildDirection", "y"],
+    ["Part_3003.8"],
+    ["Part_3039.15"],
+    ["Part_3039.16"],
+    [0, "endGroup"],
+
+    [0, "translate", "house", 10],
+
+];
+
+export default MYAPP.instCSV;
