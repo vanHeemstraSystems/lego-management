264ad2e5848ade06451ab638b80a7db402afe884
Willem van Heemstra <wvanheemstra@icloud.com>
2023-12-04 15:10:25 +0000

    Update

--- containers/app/lego/src/components/playground.tsx
+++ containers/app/lego/src/components/playground.tsx
@@ -9,7 +9,7 @@ import * as THREE from "three";
 // import * as ColladaLoader from "three-collada-loader"; // Throws duplicate use of Three
 // import * as THREE_ADDONS from "three-addons"; // Throws error
 // See https://stackoverflow.com/questions/42434398/how-to-use-threejs-collada-loader-with-typescript-angular-cli
-import { ColladaLoader } from "../../node_modules/three/examples/jsm/loaders/ColladaLoader.js";
+import { ColladaLoader } from "../../node_modules/three/examples/jsm/loaders/ColladaLoader";
 // REMOVE import * as TWEEN from "@tweenjs/tween.js";
 import { InstCSV } from "../models/instCSV.js";
 import "../../node_modules/spin.js/spin.css";
